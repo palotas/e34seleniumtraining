050d7b749d4344bf69726b1e9cd347072f19a147
Michael PalotasVM <michael.palotas@gridfusion.net>
2013-08-02 09:20:25 +0200

    changed path to new data.csv location

--- src/test/java/util/CsvDataProvider.java
+++ src/test/java/util/CsvDataProvider.java
@@ -24,7 +24,7 @@ public class CsvDataProvider {
     public static Iterator<Object[]> createData(){ 
             List<Object[]> myEntries = new ArrayList<Object[]>(); 
             try { 
-                    CSVReader reader = new CSVReader(new FileReader("/home/gridfusion/SeleniumTraining/workspace/src/test/java/util/selenium/data.csv")); 
+                    CSVReader reader = new CSVReader(new FileReader("/home/gridfusion/SeleniumTraining/Workspace/selenium/src/test/java/util/data.csv")); 
                     String[] nextLine = null; 
                     while ((nextLine = reader.readNext()) != null){ 
                             myEntries.add(new Object [] {nextLine}); 
