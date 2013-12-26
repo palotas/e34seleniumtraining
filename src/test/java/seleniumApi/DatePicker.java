fb22b497b0fdd475f45d84a6fa799db58843ea68
Michael PalotasVM <michael.palotas@gridfusion.net>
2013-12-26 12:53:08 +0100

    initial commit

---
+++ src/test/java/seleniumApi/DatePicker.java
@@ -0,0 +1,64 @@
+package seleniumApi;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class DatePicker {
+	
+	@Test
+	public void selectDate() throws InterruptedException {
+		
+		WebDriver driver=new FirefoxDriver();
+		
+		//if this is commented out, the input field cannot be found
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		
+	
+		driver.get("http://gwt.googleusercontent.com/samples/Showcase/Showcase.html#!CwDatePicker");
+		
+		//option 1
+		/*
+		WebElement datePickerTextBox=driver.findElement(By.className("gwt-DateBox"));
+		datePickerTextBox.click();	
+		WebElement cell=driver.findElement(By.xpath("/html/body/div[5]/div/table/tbody/tr[2]/td/table/tbody/tr[7]/td[3]/div"));
+		cell.click();
+		*/
+		
+		//option 2
+		WebElement datePickerTextBox=driver.findElement(By.className("gwt-DateBox"));
+		datePickerTextBox.click();
+		
+		Thread.sleep(2000);
+		
+		WebElement datePicker=driver.findElement(By.xpath("/html/body/div[5]/div/table/tbody/tr[2]/td/table/tbody"));
+		List<WebElement>rows=datePicker.findElements(By.tagName("tr"));
+		System.out.println("Rows: " + rows.size());
+		WebElement row7=rows.get(6);
+		
+		List<WebElement> cells=row7.findElements(By.tagName("td"));
+		WebElement cell31=cells.get(2);
+		WebElement div=cell31.findElement(By.tagName("div"));
+		div.click();
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+
+}
---
+++ src/test/java/seleniumApi/Radiobuttons.java
@@ -0,0 +1,59 @@
+package seleniumApi;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Radiobuttons {
+	
+	@Test
+	public void countRadioButtons() throws InterruptedException {
+		
+		WebDriver driver=new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.get("http://gwt.googleusercontent.com/samples/Showcase/Showcase.html#!CwRadioButton");
+		
+		List<WebElement> radioButtons=driver.findElements(By.className("gwt-RadioButton"));
+		Assert.assertEquals(10, radioButtons.size());
+				
+		Thread.sleep(3000);
+		driver.quit();
+	}
+	
+	
+	@Test
+	public void verifyButtonNames() throws InterruptedException {
+		
+		//set up reference data
+		List<String> refList=new ArrayList<String>(Arrays.asList("blue", "red", "yellow", "green", 
+				"Baseball", "Basketball", "Football", "Hockey", "Soccer", "Water Polo"));
+
+		int index=0;
+		
+		WebDriver driver=new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.get("http://gwt.googleusercontent.com/samples/Showcase/Showcase.html#!CwRadioButton");
+		
+		List<WebElement> radioButtons=driver.findElements(By.className("gwt-RadioButton"));
+		
+		for(WebElement radioButton : radioButtons) {
+			System.out.println("Labels of radio buttons: " + radioButton.getText());
+			Assert.assertEquals(refList.get(index), radioButton.getText());
+			index++;
+		}
+		
+		Thread.sleep(3000);
+		driver.quit();
+		
+	}
+
+}
