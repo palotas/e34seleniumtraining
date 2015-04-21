1c598f326ed61358760669f3794da888e8a06b38
Michael PalotasVM <michael.palotas@gridfusion.net>
2015-04-21 10:58:28 +0200

    added axa specific driver factory
    Former-commit-id: cd46fa5488402fc073935841116990ddd1359b86

--- src/test/java/webdriverBasics/BasicTests.java
+++ src/test/java/webdriverBasics/BasicTests.java
@@ -1,5 +1,8 @@
 package webdriverBasics;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxProfile;
@@ -19,21 +22,33 @@ public class BasicTests {
 	 * test runs with local firefox driver selenium server jar does not need to
 	 * run for this
 	 */
+
+	@Test
+	public void axaProxyAuthTestWithDriverFactory() throws FileNotFoundException, IOException {
+
+		WebDriver driver = util.AxaDriverFactory.createAxaRemoteFirefoxDriver();
+
+		// navigate to the URL 
+		driver.get("http://gridfusion.net");
+		// close the Browser
+		//driver.quit();
+}
 	
 	@Test
-public void axaProxyAuthTest() {
+	public void axaProxyAuthTest() {
 		ProfilesIni profile = new ProfilesIni();
 		FirefoxProfile ffprofile = profile.getProfile("selenium");
 		ffprofile.setPreference("signon.autologin.proxy", true);
 		ffprofile.setPreference("network.proxy.type", 1); //1=manual config, 2=pac file
 		ffprofile.setPreference("network.proxy.http", "194.40.39.31");
 		ffprofile.setPreference("network.proxy.http_port", 8080);
+		//ffprofile.setPreference("network.proxy.share_proxy_settings", true);
 		WebDriver driver = new FirefoxDriver(ffprofile);
 
 		// navigate to the URL 
 		driver.get("http://gridfusion.net");
 		// close the Browser
-		driver.quit();
+		//driver.quit();
 }
 	
 	
