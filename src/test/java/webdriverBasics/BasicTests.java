2181c00a389bb1498b76ca6474349e985c124acd
Michael PalotasVM <michael.palotas@gridfusion.net>
2015-04-21 13:12:35 +0200

    changed to axa driver factory
    Former-commit-id: 1a3116979c16eb521973a08b1fc3bc7c49f3dc63

--- src/test/java/webdriverBasics/BasicTests.java
+++ src/test/java/webdriverBasics/BasicTests.java
@@ -12,27 +12,75 @@ import org.testng.annotations.Test;
 
 public class BasicTests {
 
-	/*
-	 * This test: - creates a new Firefox Driver / open a Firefox Browser -
-	 * opens the page http://gridfusion.net - closes the browser
-	 * 
-	 * Please note: no actual verification of anything is done
-	 * 
-	 * If you install from scratch: make sure to put selenium jar into classpath
-	 * test runs with local firefox driver selenium server jar does not need to
-	 * run for this
-	 */
+	@Test
+	public void myFirstTest() {
+
+	    ProfilesIni profile = new ProfilesIni();
+        FirefoxProfile ffprofile = profile.getProfile("selenium");
+
+        ffprofile.setPreference("signon.autologin.proxy", true); 
+        ffprofile.setPreference("network.proxy.type", 1); //1=manual config, 2=pac file
+        //ffprofile.setPreference("network.proxy.http", "localhost");
+        //ffprofile.setPreference("network.proxy.http_port", 3128); 
+                
+        WebDriver driver = new FirefoxDriver(ffprofile);
+		
+		// navigate to the URL 
+		driver.get("http://gridfusion.net");
+
+		// close the Browser
+		driver.quit();
+	}
+	
 
 	@Test
 	public void axaProxyAuthTestWithDriverFactory() throws FileNotFoundException, IOException {
 
-		WebDriver driver = util.AxaDriverFactory.createAxaRemoteFirefoxDriver();
+		WebDriver driver = util.AxaDriverFactory.createAxaFirefoxDriver();
 
 		// navigate to the URL 
 		driver.get("http://www.axa.ch");
 		// close the Browser
-		//driver.quit();
+		driver.quit();
 }
+
+
+	@Test
+	public void pageTitleTest() throws InterruptedException, FileNotFoundException, IOException {
+
+		WebDriver driver = util.AxaDriverFactory.createAxaFirefoxDriver();
+		driver.get("http://gridfusion.net");
+		String pageTitle = driver.getTitle();
+		System.out.println("Page Title: " + pageTitle);
+		
+		Thread.sleep(2000);
+		driver.quit();
+	}
+	
+	
+	
+	@Test
+	public void pageTitleTestWithAssertNoTryCatch() throws FileNotFoundException, IOException {
+		WebDriver driver = util.AxaDriverFactory.createAxaFirefoxDriver();
+		driver.get("http://gridfusion.net");
+		Assert.assertEquals(driver.getTitle(), "GRIDFUSION");
+		driver.quit();
+	}
+	
+
+	@Test
+	public void pageTitleTestWithAssert() throws FileNotFoundException, IOException {
+		WebDriver driver = util.AxaDriverFactory.createAxaFirefoxDriver();
+		try {
+			driver.get("http://gridfusion.net");
+			Assert.assertEquals(driver.getTitle(), "ridfusion");			
+		}
+		finally {
+			driver.quit();			
+		}
+	}
+	
+	
 	
 	@Test
 	public void axaProxyAuthTest() {
@@ -50,70 +98,5 @@ public class BasicTests {
 		// close the Browser
 		//driver.quit();
 }
-	
-	
-	@Test
-	public void myFirstTest() {
-
-	    ProfilesIni profile = new ProfilesIni();
-        FirefoxProfile ffprofile = profile.getProfile("proxyauth");
-
-        ffprofile.setPreference("signon.autologin.proxy", true); 
-        ffprofile.setPreference("network.proxy.type", 1); //1=manual config, 2=pac file
-        //ffprofile.setPreference("network.proxy.http", "localhost");
-        //ffprofile.setPreference("network.proxy.http_port", 3128); 
-                
-        WebDriver driver = new FirefoxDriver(ffprofile);
-        
-		// create the driver and open Firefox
-		//WebDriver driver = new FirefoxDriver();
-		
-		// navigate to the URL 
-		driver.get("http://gridfusion.net");
-
-		// close the Browser
-		driver.quit();
-	}
-
-	/*
-	 * This test: - creates Firefox Driver / opens a Firefox browser - navigates
-	 * to http://gridfusion.net - gets the page title - prints out the page
-	 * title - closes the browser - note: test will always pass, as no asserts
-	 * are done
-	 */
-	@Test
-	public void pageTitleTest() throws InterruptedException {
-
-		WebDriver driver = new FirefoxDriver();
-		driver.get("http://gridfusion.net");
-		String pageTitle = driver.getTitle();
-		System.out.println("Page Title: " + pageTitle);
-		
-		Thread.sleep(2000);
-		driver.quit();
-	}
-	
-	
-	
-	@Test
-	public void pageTitleTestWithAssertNoTryCatch() {
-		WebDriver driver = new FirefoxDriver();
-		driver.get("http://gridfusion.net");
-		Assert.assertEquals(driver.getTitle(), "Gridfusion-Technology & Management Consulting");
-		driver.quit();
-	}
-	
-
-	@Test
-	public void pageTitleTestWithAssert() {
-		WebDriver driver = new FirefoxDriver();
-		try {
-			driver.get("http://gridfusion.net");
-			Assert.assertEquals(driver.getTitle(), "ridfusion-Technology & Management Consulting");			
-		}
-		finally {
-			driver.quit();			
-		}
-	}
 }
 
