ff3199a449f9aedcd26f225332c578f681c8dbea
michael palotas <michael.palotasgridfusion.net>
2015-06-17 15:49:28 +0200

    cleaned up driver factory to support IE and FF locally and remote
    Former-commit-id: 428fc3d39240b24755a45389e11e7b1547ae2b90

--- src/test/java/util/AxaDriverFactory.java
+++ src/test/java/util/AxaDriverFactory.java
@@ -15,12 +15,24 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 
 public  class AxaDriverFactory {
 	
-	static String firefoxProfile = "selenium";
-	static int proxy_type = 1;  //1=manual config, 2=pac file, 5=share system proxy settings
+	//static String firefoxProfile = "selenium";
+	//static int proxy_type = 1;  //1=manual config, 2=pac file, 5=share system proxy settings
 	
 	/* AXA proxy settings */
 	//static String proxy_http = "194.40.39.31";
 	//static int http_port = 8080;
+
+	//ProfilesIni profile = new ProfilesIni();
+	//FirefoxProfile ffprofile = profile.getProfile(firefoxProfile);
+	//ffprofile.setPreference("signon.autologin.proxy", true);
+	//ffprofile.setPreference("network.proxy.type", proxy_type); 
+	//ffprofile.setPreference("network.proxy.http", proxy_http);
+	//ffprofile.setPreference("network.proxy.http_port", http_port);
+	//ffprofile.setPreference("network.proxy.share_proxy_settings", share_proxy_settings);
+	//WebDriver driver = new FirefoxDriver(ffprofile);
+	
+	
+	
 	
 	/* local SQUID3 proxy settings */
 	static String proxy_http = "localhost";
@@ -29,42 +41,40 @@ public  class AxaDriverFactory {
 	
 	static boolean share_proxy_settings = true; //--> needs to potentially be set to false!!!
 	
-		
-public static WebDriver createAxaIEDriver() throws FileNotFoundException, IOException {
-			
-		//ProfilesIni profile = new ProfilesIni();
-		//FirefoxProfile ffprofile = profile.getProfile(firefoxProfile);
-		//ffprofile.setPreference("signon.autologin.proxy", true);
-		//ffprofile.setPreference("network.proxy.type", proxy_type); 
-		//ffprofile.setPreference("network.proxy.http", proxy_http);
-		//ffprofile.setPreference("network.proxy.http_port", http_port);
-		//ffprofile.setPreference("network.proxy.share_proxy_settings", share_proxy_settings);
-		
-		//WebDriver driver = new FirefoxDriver(ffprofile);
 	
+	//local IE driver
+	public static WebDriver createAxaIEDriver() throws FileNotFoundException, IOException {
 		System.setProperty("webdriver.ie.driver", "IEDriverServer.exe"); //this sets the path to IE driver to seleniumtraining
 		WebDriver driver = new InternetExplorerDriver();
 	    return driver;
 	}
+
+
+	//local Firefox driver
+	public static WebDriver createAxaFirefoxDriver() throws FileNotFoundException, IOException {
+
+	WebDriver driver = new FirefoxDriver();
+    return driver;
+	}
 	
-	
+	//remote IE driver
 	public static WebDriver createAxaRemoteIEDriver() throws FileNotFoundException, IOException {
 		
 		DesiredCapabilities capabilities = new DesiredCapabilities();
-		
-		//ProfilesIni profile = new ProfilesIni();
-		//FirefoxProfile ffprofile = profile.getProfile(firefoxProfile);
-		//ffprofile.setPreference("signon.autologin.proxy", true);
-		//ffprofile.setPreference("network.proxy.type", proxy_type); //1=manual config, 2=pac file
-		//ffprofile.setPreference("network.proxy.http", proxy_http);
-		//ffprofile.setPreference("network.proxy.http_port", http_port);
-		//ffprofile.setPreference("network.proxy.share_proxy_settings", share_proxy_settings);
-        
-		//capabilities.setCapability(FirefoxDriver.PROFILE, ffprofile);
 		capabilities.setBrowserName("internet explorer");
 		System.setProperty("webdriver.ie.driver", "IEDriverServer.exe"); //this sets the path to IE driver to seleniumtraining
 
 		WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capabilities);
 	    return driver;
 	}
+
+	//remote Firefox driver
+	public static WebDriver createAxaRemoteFirefoxDriver() throws FileNotFoundException, IOException {
+		
+		DesiredCapabilities capabilities = new DesiredCapabilities();
+		capabilities.setBrowserName("firefox");
+		WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capabilities);
+	    return driver;
+	}
+
 }
