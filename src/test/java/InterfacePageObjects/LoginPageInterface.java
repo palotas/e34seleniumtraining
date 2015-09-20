7aa2e7f5e2a84a631e8dbc603f345e095b213d5d
gridfusion <michael.palotas@gridfusion.net>
2015-09-20 21:12:19 +0200

    adding interfaces as a second option for page objects
    Former-commit-id: 57c0891600917473c8569b5b450dc0a26ecf5dec

---
+++ src/test/java/InterfacePageObjects/LoginPageInterface.java
@@ -0,0 +1,11 @@
+package InterfacePageObjects;
+
+/**
+ * Created by gridfusion on 20/09/15.
+ */
+public interface LoginPageInterface {
+
+    public void enterUserName(String username);
+    public void enterPassword(String password);
+    public void clickLoginButton();
+}
