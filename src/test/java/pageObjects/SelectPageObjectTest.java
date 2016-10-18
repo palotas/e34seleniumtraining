913b68f76ff261d32305f91754db278ac00c070e
e34 <michael.palotas@element34.net>
2016-10-18 15:00:57 +0200

    updated latest tests

--- src/test/java/pageObjects/SelectPageObjectTest.java
+++ src/test/java/pageObjects/SelectPageObjectTest.java
@@ -3,6 +3,7 @@ package pageObjects;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import java.io.FileNotFoundException;
@@ -13,12 +14,32 @@ import java.io.IOException;
  */
 public class SelectPageObjectTest {
 
+    @BeforeTest
+    public void setup() {
+        String OS = System.getProperty("os.name");
+
+        switch (OS) {
+            case "Linux":
+                System.setProperty("webdriver.gecko.driver","/home/e34/Downloads/geckodriver");
+                System.setProperty("webdriver.chrome.driver", "/home/e34/Downloads/chromedriver");
+                break;
+
+            case "Mac OS X":
+                System.setProperty("webdriver.gecko.driver","/Users/gridfusion/Downloads/geckodriver");
+                System.setProperty("webdriver.chrome.driver", "/Users/gridfusion/Downloads/chromedriver");
+                break;
+
+            default:
+                System.out.println(System.getProperty("os.name") + " is not supported ");
+                break;
+        }
+    }
+
 
     @Test
-    public void testpageTest() throws InterruptedException, FileNotFoundException, IOException {
+    public void testpageTest() throws InterruptedException, IOException {
 
         DesiredCapabilities capabilities = DesiredCapabilities.firefox();
-        capabilities.setCapability("marionette", false);
         WebDriver driver = new FirefoxDriver(capabilities);
 
         driver.get("http://www.element34.net/testpage");
