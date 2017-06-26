3f28d1573292d28ef90b7e18c812db50424fb075
palotas <michael.palotas@element34.com>
2017-06-26 17:52:11 +0200

    refactored testng package

--- src/test/java/testng/TestngDependency.java
+++ src/test/java/testng/TestngDependency.java
@@ -13,27 +13,12 @@ public class TestngDependency {
 		int c;
 		c=a + b;
 		System.out.println("mytest1...");		
-		Assert.assertTrue(c==5); //this test will fail
+		Assert.assertTrue(c==3);
 
 	}
 	
 	@Test(dependsOnMethods={"mytest1"})
 	public void mytest2() {
 		System.out.println("mytest2...");	
-		
-		//force test to pass -> do not do this in "real" life
-		Assert.assertTrue(true); 
-	}	
-	
-    @Test
-    public void mytest3() {
-		System.out.println("mytest3...");	
-		Assert.assertTrue(true);
-	}	
-    
-    @Test
-	public void mytest4() {
-		System.out.println("mytest4...");	
-		Assert.assertTrue(true);
-	}	
+	}
 }
