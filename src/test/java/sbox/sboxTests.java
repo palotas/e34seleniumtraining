6d20450e7bea888494ff15c6a7911e1c62a06993
palotas <michael.palotas@element34.com>
2017-07-31 21:54:34 +0200

    fixed hub url and added sbox features

--- src/test/java/sbox/sboxTests.java
+++ src/test/java/sbox/sboxTests.java
@@ -19,13 +19,14 @@ public class sboxTests {
 		capability.setCapability("video", true);
 		capability.setBrowserName("chrome");
 		capability.setVersion("n-2");
-		RemoteWebDriver driver = new RemoteWebDriver(new URL("https://789b1ea7eca7.element34.net/wd/hub"), capability);
-		//RemoteWebDriver driver = new RemoteWebDriver(new URL("https://vm-106.element34.net:4444/wd/hub"), capability);
+		//RemoteWebDriver driver = new RemoteWebDriver(new URL("https://789b1ea7eca7.element34.net/wd/hub"), capability);
+		RemoteWebDriver driver = new RemoteWebDriver(new URL("https://vm-106.element34.net:443/wd/hub"), capability);
 
 		driver.get("http://www.google.com");
 		System.out.println(driver.getTitle());
 		//System.out.println("Video URL: " + "https://789b1ea7eca7.element34.net/e34/api/video?session=" + driver.getSessionId());
-		//System.out.println("Video URL: " + "http://vm-105.element34.net:8080/videos/" + driver.getSessionId() + ".mp4");
+		System.out.println("Video URL: " + "https://vm-106.element34.net/videos/" + driver.getSessionId() + ".mp4");
+		Thread.sleep(5000);
 		driver.quit();
 	}
 
