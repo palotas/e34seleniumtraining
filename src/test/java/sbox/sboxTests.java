5f4e8437e3ff0884b2a2f8eb8d9e26bbefff3f37
palotas <michael.palotas@element34.com>
2017-10-12 10:54:45 +0200

    updating sbox tests and custom wait

--- src/test/java/sbox/sboxTests.java
+++ src/test/java/sbox/sboxTests.java
@@ -1,8 +1,12 @@
 package sbox;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
@@ -15,7 +19,7 @@ import java.net.URL;
 
 public class sboxTests {
 
-	private final String host = "vm-106.element34.net";
+	private final String host = "192.168.1.178";
 
 	//@BeforeTest
 	//public void setup() {
@@ -29,21 +33,13 @@ public class sboxTests {
 				{""},
 				{"n-1"},
 				{"n-2"},
-				{"n-5"}
+				{"n-3"}
 		};
 	}
 
 	//browsers and versions are hardcoded on the data provider
 	@DataProvider(name = "browserProvider", parallel = true)
 	public Object[][] getDrivers() {
-		DesiredCapabilities firefox1 = DesiredCapabilities.firefox();
-		firefox1.setCapability("version", "52");
-
-		DesiredCapabilities firefox2 = DesiredCapabilities.firefox();
-		firefox2.setCapability("version", "51");
-
-		DesiredCapabilities firefox3 = DesiredCapabilities.firefox();
-		firefox3.setCapability("version", "50");
 
 		DesiredCapabilities chrome1 = DesiredCapabilities.chrome();
 		chrome1.setCapability("version", "58");
@@ -54,14 +50,18 @@ public class sboxTests {
 		DesiredCapabilities chrome3 = DesiredCapabilities.chrome();
 		chrome3.setCapability("version", "56");
 
+		DesiredCapabilities internetExplorer = DesiredCapabilities.internetExplorer();
+
+		DesiredCapabilities edge = DesiredCapabilities.edge();
+
+
 		return new Object[][]{
-				{firefox1},
-				{firefox2},
-				{firefox3},
 				{chrome1},
 				{chrome2},
 				{chrome3},
-    };
+				{internetExplorer},
+				{edge},
+    	};
 	}
 
 
@@ -99,7 +99,7 @@ public class sboxTests {
 
 
 
-	@Test(dataProvider = "chromeVersions")
+	@Test(dataProvider = "chromeVersions", invocationCount = 1)
 	public void chromeWithDifferentVersionsTest(String version) throws IOException, InterruptedException {
 
 		DesiredCapabilities capability = new DesiredCapabilities();
@@ -108,33 +108,34 @@ public class sboxTests {
 		capability.setVersion(version);
 
 		//replace URL with company specific entry point
-		RemoteWebDriver driver = new RemoteWebDriver(new URL("https://vm-106.element34.net:443/wd/hub"), capability);
+		//RemoteWebDriver driver = new RemoteWebDriver(new URL("https://789b1ea7eca8.element34.net:443/wd/hub"), capability);
+		RemoteWebDriver driver = new RemoteWebDriver(new URL("https://vm-105.element34.net:443/wd/hub"), capability);
 
 		System.out.println("Browser version: " + driver.getCapabilities().getBrowserName() + " " + driver.getCapabilities().getVersion());
 
 		//replace with company specific URL
 		driver.get("http://www.google.com");
-		System.out.println("Video URL: " + "https://vm-106.element34.net/videos/" + driver.getSessionId() + ".mp4");
+		System.out.println("Video URL: " + "https://vm-105.element34.net/videos/" + driver.getSessionId() + ".mp4");
 
 		//leave browser open for 5 seconds and close browser afterwards
 		Thread.sleep(5000);
 		driver.quit();
 	}
 
-	@Test(dataProvider = "browserProviderWithoutVersions")
+	@Test(dataProvider = "browserProvider", invocationCount = 1)
 	public void multiBrowserVersionTest(DesiredCapabilities caps) throws IOException, InterruptedException {
 
 		//enable video recording
 		caps.setCapability("video", true);
 
 		//replace URL with company specific entry point
-		RemoteWebDriver driver = new RemoteWebDriver(new URL("https://vm-106.element34.net:443/wd/hub"), caps);
-		System.out.println("Live View URL > http://vm-106.element34.net:4444/ui/liveview?session=" + driver.getSessionId().toString());
+		RemoteWebDriver driver = new RemoteWebDriver(new URL("https://vm-105.element34.net:443/wd/hub"), caps);
+		System.out.println("Live View URL > http://vm-105.element34.net:4444/ui/liveview?session=" + driver.getSessionId().toString());
 
 
 		//replace with company specific URL
-		driver.get("http://www.google.com");
-		System.out.println("Video URL - " + driver.getCapabilities().getBrowserName() + " " + driver.getCapabilities().getVersion() + " : " + "https://vm-106.element34.net/videos/" + driver.getSessionId() + ".mp4");
+		driver.get("http://www.westpac.com.au");
+		System.out.println("Video URL - " + driver.getCapabilities().getBrowserName() + " " + driver.getCapabilities().getVersion() + " : " + "https://vm-105.element34.net/videos/" + driver.getSessionId() + ".mp4");
 
 		//leave browser open for 5 seconds and close browser afterwards
 		Thread.sleep(5000);
@@ -142,19 +143,33 @@ public class sboxTests {
 	}
 
 
-	@Test
+	@Test(invocationCount = 1, threadPoolSize = 5)
 	public void singleTest() throws IOException, InterruptedException {
 
 		DesiredCapabilities capability = new DesiredCapabilities();
 		capability.setCapability("video", true);
-		capability.setBrowserName("firefox");
-		RemoteWebDriver driver = new RemoteWebDriver(new URL("https://vm-106.element34.net:443/wd/hub"), capability);
+		capability.setBrowserName("chrome");
+		capability.setCapability("l_testName", "test12345");
+		RemoteWebDriver driver = new RemoteWebDriver(new URL("https://vm-105.element34.net:443/wd/hub"), capability);
 		printLiveViewURL(driver);
 		printVideoURL(driver);
 
+		driver.get("https://stackoverflow.com/questions/27241186/how-to-determine-when-document-has-loaded-after-loading-external-css");
 
-		driver.get("http://www.axa.de");
-		Thread.sleep(5000);
+
+		for(int i= 0; i < 300; i++){
+			driver.getTitle();
+			driver.getCurrentUrl();
+			driver.get("https://google.com");
+
+			JavascriptExecutor js = (JavascriptExecutor) driver;
+			System.out.println(js.executeScript("return document.readyState"));
+			//System.out.println(js.executeScript("return jQuery.active"));
+			Thread.sleep(250);
+			driver.get("https://stackoverflow.com/questions/27241186/how-to-determine-when-document-has-loaded-after-loading-external-css");
+
+		}
+
 
 		driver.quit();
 	}
@@ -166,7 +181,7 @@ public class sboxTests {
 
 		DesiredCapabilities capability = new DesiredCapabilities();
 		capability.setCapability("video", true);
-		capability.setBrowserName("firefox");
+		capability.setBrowserName("chrome");
 		RemoteWebDriver driver = new RemoteWebDriver(new URL("https://vm-106.element34.net:443/wd/hub"), capability);
 
 		driver.get("http://the-internet.herokuapp.com/download");
@@ -229,5 +244,62 @@ public class sboxTests {
 	private void printLiveViewURL(RemoteWebDriver driver) {
 		System.out.println("Live View URL > http://vm-106.element34.net:4444/ui/liveview?session=" + driver.getSessionId().toString());
 	}
+
+
+	@Test
+	public void ciscoTest() {
+			RemoteWebDriver driver = null;
+			String version = "47";
+			DesiredCapabilities dc = new DesiredCapabilities();
+			dc.setCapability("browserName", "chrome");
+			//dc.setCapability("version", "49");
+			//dc.setCapability("auth","token");
+			dc.setCapability("video", true);
+			try {
+				//driver = new RemoteWebDriver(new URL("http://seleniumbox.cisco.com/wd/hub"), dc);
+				driver = new RemoteWebDriver(new URL("https://vm-105.element34.net/wd/hub"), dc);
+			}
+			catch (MalformedURLException e) {
+				System.out.println(e);
+			}
+
+			try {
+				driver.manage().window().setSize(new Dimension(1024, 768)); //Works fine with firefox v47
+				System.out.println("window.setSize working");
+			}
+			catch(Exception e) {
+				e.printStackTrace();
+//           Raises following exception in case of firefox v52:
+//           org.openqa.selenium.WebDriverException: setWindowRect
+//           Build info: version: '3.4.0', revision: 'unknown', time: 'unknown'
+//           System info: host: 'c64219d7e34d', ip: '172.17.0.4', os.name: 'Linux', os.arch: 'amd64', os.version: '3.10.0-327.36.3.el7.x86_64', java.version: '1.8.0_111'
+//           Driver info: org.openqa.selenium.firefox.FirefoxDriver
+//           Capabilities [{moz:profile=/tmp/rust_mozprofile.tYnC2vOIbWOn, rotatable=false, timeouts={implicit=0.0, page load=300000.0, script=30000.0}, pageLoadStrategy=normal, platform=ANY, specificationLevel=0.0, moz:accessibilityChecks=false, acceptInsecureCerts=false, browserVersion=52.0.2, platformVersion=3.10.0-327.36.3.el7.x86_64, moz:processID=91.0, browserName=firefox, javascriptEnabled=true, platformName=linux}]
+
+			}
+			try { //Works fine with firefox v47
+				System.out.println(String.format("%s LiveView: ","DEMO",driver.getSessionId()));
+
+				driver.get("https://github.com/SeleniumHQ");
+				//Thread.sleep(5000); //Allow to open live view link
+				Actions action = new Actions(driver);
+				WebElement elem = driver.findElement(By.xpath("//a[contains(@href, '/login')]"));
+	            action.moveToElement(elem).perform();
+				//Thread.sleep(10000); //Allow to see
+			}
+			catch(Exception e) {
+				System.out.println("moveToElement did not work.");
+				System.out.println(e);
+//            Raises following exception in case of firefox v52:
+//            Caused by: org.openqa.selenium.UnsupportedCommandException: mouseMoveTo
+//            Build info: version: '3.4.0', revision: 'unknown', time: 'unknown'
+//            System info: host: '3ef0e26b9d0f', ip: '172.17.0.4', os.name: 'Linux', os.arch: 'amd64', os.version: '3.10.0-327.36.3.el7.x86_64', java.version: '1.8.0_111'
+//            Driver info: driver.version: RemoteWebDriver
+			}
+			finally {
+				driver.quit();
+			}
+		}
+
 
 }
--- src/test/java/wait/CustomWait.java
+++ src/test/java/wait/CustomWait.java
@@ -1,15 +1,20 @@
 package wait;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.function.Function;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
 
 public class CustomWait {
 
@@ -58,4 +63,53 @@ public class CustomWait {
 			driver.quit();
 		}
 	}
+
+	@Test
+	public void fluentWait() throws MalformedURLException {
+		DesiredCapabilities capability = new DesiredCapabilities();
+		capability.setBrowserName("chrome");
+		WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capability);
+
+
+
+
+		driver.get("about:policy");
+
+		Wait<WebDriver> wait = new FluentWait<WebDriver>(driver)
+				.withTimeout(30, SECONDS)
+				.pollingEvery(5, SECONDS)
+				//.ignoring(NoSuchElementException.class);
+				.ignoring(StaleElementReferenceException.class);
+
+		WebElement reload = wait.until(new Function<WebDriver, WebElement>()
+		{
+
+			@Override
+			public WebElement apply(WebDriver driver) {
+				return driver.findElement(By.id("reload-policies"));
+			}
+		});
+
+
+		reload = driver.findElement(By.id("reload-policies"));
+		System.out.println(reload.getText());
+		driver.get("about:policy");
+		System.out.println(reload.getText());
+		System.out.println(reload.getText());
+		driver.quit();
+
+
+
+
+
+	}
+	// Waiting 30 seconds for an element to be present on the page, checking
+
+	// for its presence once every 5 seconds.
+
+
+
+
+
+
 }
