f9806a1b4b6d6ae28fd60d81c49198fbcef84b99
palotas <michael.palotas@element34.com>
2017-10-12 20:19:08 +0200

    added BaseTest for setting up webdriver properties

---
+++ src/test/java/elnadv/BaseTest.java
@@ -0,0 +1,48 @@
+package elnadv;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import static elnadv.Helpers.sleepTight;
+
+/**
+ * Created by e34 on 13/10/2017.
+ */
+public class BaseTest {
+
+    @BeforeSuite
+    public void setup() {
+        String OS = System.getProperty("os.name");
+        System.out.println("setting up webdriver properties");
+
+        switch (OS) {
+            case "Linux":
+                System.setProperty("webdriver.gecko.driver","/home/e34/Downloads/geckodriver");
+                System.setProperty("webdriver.chrome.driver", "/home/e34/Downloads/chromedriver");
+                break;
+
+            case "Mac OS X":
+                System.setProperty("webdriver.gecko.driver","/Users/gridfusion/Downloads/geckodriver");
+                System.setProperty("webdriver.chrome.driver", "/Users/gridfusion/Downloads/chromedriver");
+                break;
+
+            case "Windows 10":
+                System.setProperty("webdriver.gecko.driver","C:\\Users\\mpalotas\\IdeaProjects\\e34seleniumtraining\\resources\\geckodriver.exe");
+                System.setProperty("webdriver.chrome.driver", "C:\\Users\\mpalotas\\IdeaProjects\\e34seleniumtraining\\resources\\chromedriver.exe");
+                break;
+
+            default:
+                System.out.println(System.getProperty("os.name") + " is not supported ");
+                break;
+        }
+    }
+}
