d9e5542b616d085e9d36ae9aa515ed0d6ebae71f
palotas <michael.palotas@element34.com>
2017-10-18 10:26:50 +0200

    moved more data providers to separate class

--- src/test/java/sbox/TestData.java
+++ src/test/java/sbox/TestData.java
@@ -1,11 +1,14 @@
 package sbox;
 
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.DataProvider;
+
 import static sbox.Helpers.getAllChromes;
 
 public class TestData {
 
-    @org.testng.annotations.DataProvider(name = "chromeVersions", parallel = true)
-    public Object[][] createData1() {
+    @DataProvider(name = "chromeVersions", parallel = true)
+    public Object[][] createVersions1() {
         return new Object[][] {
                 {""},
                 {"n-1"},
@@ -14,8 +17,66 @@ public class TestData {
         };
     }
 
-    @org.testng.annotations.DataProvider(name = "chromeVersions2", parallel = true)
-    public Object[][] offsets() {
+    @DataProvider(name = "chromeVersions2", parallel = true)
+    public Object[][] createVersions2() {
         return getAllChromes();
+    }
+
+    @DataProvider(name = "browserProvider", parallel = true)
+    public Object[][] getDrivers() {
+
+        DesiredCapabilities chrome1 = DesiredCapabilities.chrome();
+        chrome1.setCapability("version", "58");
+
+        DesiredCapabilities chrome2 = DesiredCapabilities.chrome();
+        chrome2.setCapability("version", "57");
+
+        DesiredCapabilities chrome3 = DesiredCapabilities.chrome();
+        chrome3.setCapability("version", "56");
+
+        DesiredCapabilities internetExplorer = DesiredCapabilities.internetExplorer();
+
+        DesiredCapabilities edge = DesiredCapabilities.edge();
+
+
+        return new Object[][]{
+                {chrome1},
+                {chrome2},
+                {chrome3},
+                {internetExplorer},
+                {edge},
+        };
+    }
+
+    //no hardcoded versions. Sliding window depending on what is configured on SBOX as LATEST
+    @DataProvider(name = "browserProviderWithoutVersions", parallel = true)
+    public Object[][] getDriversWithoutVersion() {
+
+        //if no version is set, then latest browser version is used
+        DesiredCapabilities firefox1 = DesiredCapabilities.firefox();
+
+        //use n-1, n-1 etc. to specify non-hardcoded browserversions (sliding window)
+        DesiredCapabilities firefox2 = DesiredCapabilities.firefox();
+        firefox1.setCapability("version", "n-1");
+
+        DesiredCapabilities firefox3 = DesiredCapabilities.firefox();
+        firefox1.setCapability("version", "n-2");
+
+        DesiredCapabilities chrome1 = DesiredCapabilities.chrome();
+
+        DesiredCapabilities chrome2 = DesiredCapabilities.chrome();
+        chrome2.setCapability("version", "n-1");
+
+        DesiredCapabilities chrome3 = DesiredCapabilities.chrome();
+        chrome3.setCapability("version", "n-2");
+
+        return new Object[][]{
+                {firefox1},
+                {firefox2},
+                {firefox3},
+                {chrome1},
+                {chrome2},
+                {chrome3},
+        };
     }
 }
