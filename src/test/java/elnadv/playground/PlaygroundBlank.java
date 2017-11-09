a24fac011bf62e6f6491fa8d9e2f25ce048f428f
palotas <michael.palotas@element34.com>
2017-11-09 09:27:57 +0100

    adding empty class as a starting point

---
+++ src/test/java/elnadv/playground/PlaygroundBlank.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2014 - 2017.  Element34 Solutions - All Rights Reserved
+ * Unauthorized copying and redistribution of this file or parts thereof,
+ * via any medium is strictly prohibited without explicit consent of Element34 Solutions GmbH.
+ */
+
+package elnadv.playground;
+
+import org.testng.annotations.Test;
+
+public class PlaygroundBlank {
+
+    @Test
+    public void myTest() {
+
+    }
+}
