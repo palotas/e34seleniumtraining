55df1b52fa3c147bf734fe4e9214be86b800fc8f
palotas <michael.palotas@element34.com>
2017-11-26 16:48:49 +0100

    adding mobile emulation

---
+++ src/test/java/elnadv/mobile/MobileEmulation.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2014 - 2017.  Element34 Solutions - All Rights Reserved
+ * Unauthorized copying and redistribution of this file or parts thereof,
+ * via any medium is strictly prohibited without explicit consent of Element34 Solutions GmbH.
+ */
+
+package elnadv.mobile;
+
+import elnadv.BaseTest;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MobileEmulation extends BaseTest {
+
+    @Test
+    public void mobile() {
+
+        Map<String, String> mobileEmulation = new HashMap<>();
+        mobileEmulation.put("deviceName", "iPhone 6");
+
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.setExperimentalOption("mobileEmulation", mobileEmulation);
+        WebDriver driver = new ChromeDriver(chromeOptions);
+        driver.get("https://raiffeisen.ch");
+    }
+}
