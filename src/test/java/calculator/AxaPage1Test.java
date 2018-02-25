d6edf8d5d095f187ac49317de7f22205c15a2a01
palotas <michael.palotas@element34.com>
2018-02-25 10:09:54 +0100

    axa flow ok

--- src/test/java/calculator/AxaPage1Test.java
+++ src/test/java/calculator/AxaPage1Test.java
@@ -7,6 +7,7 @@
 package calculator;
 
 import elnadv.BaseTest;
+import org.omg.CORBA.TIMEOUT;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -18,17 +19,26 @@ import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.concurrent.TimeUnit;
 
 
 public class AxaPage1Test extends BaseTest {
 	
 	@Test(dataProvider="data", dataProviderClass = ReadExcel.class)
-	public void mytest1(String data_year, String data_month, String data_marke, String data_model, String data_schaltung, String data_treibstoff, String data_ps, String specificModel, String data_zusatzausruestung, String data_besKontrollschild, String data_leasing, String data_kilometer, String data_kaufjahr, String data_parkschaden, String data_notbrems) throws InterruptedException, MalformedURLException {
+	public void mytest1(String data_year, String data_month, String data_marke, String data_model,
+                        String data_schaltung, String data_treibstoff, String data_ps, String specificModel,
+                        String data_zusatzausruestung, String data_besKontrollschild, String data_leasing,
+                        String data_kilometer, String data_kaufjahr, String data_parkschaden, String data_notbrems,
+                        String data_gebDatum, String data_nationalitaet, String data_plz, String data_geschlecht,
+                        String data_entzug, String data_bisherigeVersicherer, String data_versicherer, String data_kuendigung,
+                        String data_schaden5Jahre) throws InterruptedException, MalformedURLException {
 
 		DesiredCapabilities capability = DesiredCapabilities.chrome();
 		capability.setCapability("e34:token", "72aa4d82");
 		capability.setCapability("e34:l_testName", "Selenium Test");
-		RemoteWebDriver driver = new RemoteWebDriver(new URL("https://vm-106.element34.net/wd/hub"), capability);
+        RemoteWebDriver driver = new RemoteWebDriver(new URL("https://vm-106.element34.net/wd/hub"), capability);
+        //RemoteWebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capability);
+
 
 		AxaPage1 page1 = new AxaPage1(driver);
 		page1.loadPage(driver);
@@ -51,7 +61,16 @@ public class AxaPage1Test extends BaseTest {
 		page2.selectPurchaseYear(data_kaufjahr);
 		page2.setParkschaden(data_parkschaden);
 		page2.setNotbremsassistent(data_notbrems);
-		//Thread.sleep(3000);
+		page2.setGebDatum(data_gebDatum);
+		page2.selectNationality(data_nationalitaet);
+		page2.enterPLZ(data_plz);
+		page2.setGeschlecht(data_geschlecht);
+		page2.setEntzug(data_entzug);
+		page2.setBisherigerVersicherer(data_bisherigeVersicherer, data_versicherer);
+		page2.setKuendigung(data_kuendigung);
+		page2.setSchaden(data_schaden5Jahre);
+		page2.clickWeiter();
+		Thread.sleep(3000);
 		driver.quit();
 	}
 
--- src/test/java/calculator/AxaPage2.java
+++ src/test/java/calculator/AxaPage2.java
@@ -54,6 +54,47 @@ public class AxaPage2 {
     @FindBy(id="fl_auto_hff_geburtsdatum")
     private WebElement geburtsdatum;
 
+    @FindBy(id="fl_hff_nationaliatet")
+    private WebElement nationalitaet;
+
+    @FindBy(id="fl_hff_plz")
+    private WebElement plz;
+
+    @FindBy(css="#fl_hff_geschlecht_option > label:nth-child(2)")
+    private WebElement geschlechtMann;
+
+    @FindBy(css="#fl_hff_geschlecht_option > label:nth-child(4)")
+    private WebElement geschlechtFrau;
+
+    @FindBy(css="#fl_hff_fuehrerausweisentzug_option > label:nth-child(2)")
+    private WebElement entzugJa;
+
+    @FindBy(css="#fl_hff_fuehrerausweisentzug_option > label:nth-child(4)")
+    private WebElement entzugNein;
+
+    @FindBy(css="#fl_hff_vorversicherer_option > label:nth-child(2)")
+    private WebElement bisherigerVersichererJa;
+
+    @FindBy(css="#fl_hff_vorversicherer_option > label:nth-child(4)")
+    private WebElement bisherigerVersichererNein;
+
+    @FindBy(id="fl_hff_vorversicherer_name")
+    private WebElement versicherer;
+
+    @FindBy(css="#fl_hff_vorversicherer_kuendigung_option > label:nth-child(2)")
+    private WebElement kuendigungJa;
+
+    @FindBy(css="#fl_hff_vorversicherer_kuendigung_option > label:nth-child(4)")
+    private WebElement kuendigungNein;
+
+    @FindBy(css="#fl_hff_schaeden5jahre_option > label:nth-child(2)")
+    private WebElement schadenJa;
+
+    @FindBy(css="#fl_hff_schaeden5jahre_option > label:nth-child(4)")
+    private WebElement schadenNein;
+
+    @FindBy(id="angaben_weiter")
+    private WebElement weiterbutton;
 
     public AxaPage2(WebDriver driver) {
         PageFactory.initElements(driver, this);
@@ -138,5 +179,114 @@ public class AxaPage2 {
             default:
                 System.out.println("your case does not exist");
         }
+    }
+
+    public void setGebDatum(String data_gebDatum) {
+        geburtsdatum.sendKeys(data_gebDatum);
+    }
+
+    public void selectNationality(String data_nationality) throws InterruptedException {
+
+        Thread.sleep(1000);
+        Select realSelect = new Select(nationalitaet);
+        realSelect.selectByVisibleText(data_nationality);
+    }
+
+    public void enterPLZ(String data_plz) throws InterruptedException {
+        Thread.sleep(1000);
+        plz.sendKeys(data_plz);
+    }
+
+    public void setGeschlecht(String data_geschlecht) throws InterruptedException {
+        Thread.sleep(1000);
+        switch (data_geschlecht) {
+            case "Mann" :
+                geschlechtMann.click();
+                break;
+
+            case "Frau" :
+                geschlechtFrau.click();
+                break;
+
+            default:
+                System.out.println("your case does not exist");
+        }
+    }
+
+    public void setEntzug(String data_entzug) throws InterruptedException {
+
+        Thread.sleep(1000);
+        switch (data_entzug) {
+            case "ja" :
+                entzugJa.click();
+                break;
+
+            case "nein" :
+                entzugNein.click();
+                break;
+
+            default:
+                System.out.println("your case does not exist");
+        }
+    }
+
+
+    public void setBisherigerVersicherer(String data_bisherigerVersicherer, String data_versicherer) throws InterruptedException {
+
+        Thread.sleep(1000);
+        switch (data_bisherigerVersicherer) {
+            case "ja" :
+                bisherigerVersichererJa.click();
+                Thread.sleep(500);
+
+                Select realSelect = new Select(versicherer);
+                realSelect.selectByVisibleText(data_versicherer);
+                break;
+
+            case "nein" :
+                bisherigerVersichererNein.click();
+                break;
+
+            default:
+                System.out.println("your case does not exist");
+        }
+    }
+
+    public void setKuendigung(String data_kuendigung) throws InterruptedException {
+
+        Thread.sleep(2000);
+        switch (data_kuendigung) {
+            case "ja" :
+                kuendigungJa.click();
+                break;
+
+            case "nein" :
+                kuendigungNein.click();
+                break;
+
+            default:
+                System.out.println("your case does not exist");
+        }
+    }
+
+    public void setSchaden(String data_schaden) throws InterruptedException {
+
+        Thread.sleep(1000);
+        switch (data_schaden) {
+            case "ja" :
+                schadenJa.click();
+                break;
+
+            case "nein" :
+                schadenNein.click();
+                break;
+
+            default:
+                System.out.println("your case does not exist");
+        }
+    }
+
+    public void clickWeiter() {
+        weiterbutton.click();
     }
 }
