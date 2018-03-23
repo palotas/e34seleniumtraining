3a0d3324c89f1ebf9c6a83ff4de010e7dd4f90e9
palotas <michael.palotas@element34.com>
2018-03-23 19:46:53 +0100

    moving to new class

--- src/test/java/elnadv/iframe/IFrame.java
+++ src/test/java/elnadv/iframe/IFrame.java
@@ -4,7 +4,7 @@
  * via any medium is strictly prohibited without explicit consent of Element34 Solutions GmbH.
  */
 
-package elnadv.hover;
+package elnadv.iframe;
 
 import elnadv.BaseTest;
 import org.openqa.selenium.By;
