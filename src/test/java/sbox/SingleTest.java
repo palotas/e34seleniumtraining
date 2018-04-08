b7b0fbb0e5c0852765f6b95a2d1065693f21cf7b
palotas <michael.palotas@element34.com>
2018-04-08 20:57:48 +0200

    adding test for ci

---
+++ src/test/java/sbox/SingleTest.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2014 - 2018.  Element34 Solutions - All Rights Reserved
+ * Unauthorized copying and redistribution of this file or parts thereof,
+ * via any medium is strictly prohibited without explicit consent of Element34 Solutions GmbH.
+ */
+
+package sbox;
+
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.net.URL;
+
+import static sbox.Settings.HUB;
+
+public class SingleTest {
+
+    @Test
+    public void ciscoFF() throws IOException, InterruptedException {
+
+
+        FirefoxOptions options = new FirefoxOptions();
+        options.addPreference("browser.link.open_newwindow", 3);
+
+        RemoteWebDriver driver = new RemoteWebDriver(new URL(HUB + "/wd/hub"), options);
+//		RemoteWebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), options);
+        driver.get("about:support");
+        driver.getTitle();
+
+        driver.quit();
+    }
+}
