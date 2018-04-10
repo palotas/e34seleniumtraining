b79680b3f1eb17fabbf8efa3776b2797d7b8e62b
palotas <michael.palotas@element34.com>
2018-04-10 15:41:55 +0200

    wip

--- src/test/java/sbox/CiTests.java
+++ src/test/java/sbox/CiTests.java
@@ -30,48 +30,48 @@ import static sbox.Settings.HUB;
 @Listeners(StatusListenerSbox.class)
 public class CiTests extends TestBaseThreadSafe {
 
-    @Test
-    public void ciDemo() throws IOException, InterruptedException {
-
-
-        WebDriver driver = getDriver();
-        WebDriverWait wait =  new WebDriverWait(driver, 10);
-        driver.manage().window().maximize();
-
-        try {
-            driver.get("https://www.newyorkfed.org/");
-            WebElement searchbox = driver.findElement(By.id("searchbox"));
-            searchbox.clear();
-            searchbox.sendKeys("interest rates");
-            searchbox.sendKeys(Keys.ENTER);
-
-            wait.until(ExpectedConditions.titleIs("Search - FEDERAL RESERVE BANK of NEW YORK"));
-            Assert.assertEquals(driver.getCurrentUrl(), "https://www.newyorkfed.org/search?text=interest+rates&application=ny_pub&sources=ny_pub" );
-        }
-        finally {
-            logVideoUrl((RemoteWebDriver) driver);
-            Thread.sleep(5000);
-            driver.quit();
-        }
-
-
-    }
-
-    @Test(dataProvider = "urls", dataProviderClass = TestData.class, invocationCount = 5, threadPoolSize = 100)
-    public void loadTest(String url) throws IOException, InterruptedException {
-
-
-        DesiredCapabilities capability = DesiredCapabilities.chrome();
-        capability.setCapability("version", "65");
-
-        RemoteWebDriver driver = new RemoteWebDriver(new URL("https://vm-105.element34.net/wd/hub"), capability);
-        driver.manage().window().maximize();
-        driver.get(url);
-        System.out.println(driver.getTitle());
-        Thread.sleep((long)(Math.random() * 20000));
-
-        driver.quit();
-    }
+//    @Test
+//    public void ciDemo() throws IOException, InterruptedException {
+//
+//
+//        WebDriver driver = getDriver();
+//        WebDriverWait wait =  new WebDriverWait(driver, 10);
+//        driver.manage().window().maximize();
+//
+//        try {
+//            driver.get("https://www.newyorkfed.org/");
+//            WebElement searchbox = driver.findElement(By.id("searchbox"));
+//            searchbox.clear();
+//            searchbox.sendKeys("interest rates");
+//            searchbox.sendKeys(Keys.ENTER);
+//
+//            wait.until(ExpectedConditions.titleIs("Search - FEDERAL RESERVE BANK of NEW YORK"));
+//            Assert.assertEquals(driver.getCurrentUrl(), "https://www.newyorkfed.org/search?text=interest+rates&application=ny_pub&sources=ny_pub" );
+//        }
+//        finally {
+//            logVideoUrl((RemoteWebDriver) driver);
+//            Thread.sleep(5000);
+//            driver.quit();
+//        }
+//
+//
+//    }
+//
+//    @Test(dataProvider = "urls", dataProviderClass = TestData.class, invocationCount = 5, threadPoolSize = 100)
+//    public void loadTest(String url) throws IOException, InterruptedException {
+//
+//
+//        DesiredCapabilities capability = DesiredCapabilities.chrome();
+//        capability.setCapability("version", "65");
+//
+//        RemoteWebDriver driver = new RemoteWebDriver(new URL("https://vm-105.element34.net/wd/hub"), capability);
+//        driver.manage().window().maximize();
+//        driver.get(url);
+//        System.out.println(driver.getTitle());
+//        Thread.sleep((long)(Math.random() * 20000));
+//
+//        driver.quit();
+//    }
 
 
     @Test
