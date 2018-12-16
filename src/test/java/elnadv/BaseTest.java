c09276a87cc2935474df1eb223df6e465c731bc7
Michael Palotas <michael.palotas@element34.com>
2018-12-16 20:48:12 +0100

    fix path

--- src/test/java/elnadv/BaseTest.java
+++ src/test/java/elnadv/BaseTest.java
@@ -32,8 +32,8 @@ public class BaseTest {
 
         switch (OS) {
             case "Linux":
-                System.setProperty("webdriver.gecko.driver","/home/e34/workspace/seleniumtraining/resources/linux/geckodriver");
-                System.setProperty("webdriver.chrome.driver", "/home/e34/workspace/seleniumtraining/resources/linux/chromedriver");
+                System.setProperty("webdriver.gecko.driver","/home/e34/workspace/e34seleniumtraining/resources/linux/geckodriver");
+                System.setProperty("webdriver.chrome.driver", "/home/e34/workspace/e34seleniumtraining/resources/linux/chromedriver");
                 break;
 
             case "Mac OS X":
