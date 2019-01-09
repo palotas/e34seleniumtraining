02cdfdc8e6600ee639dcec0207c02fb65e92e775
palotas <michael.palotas@element34.com>
2019-01-09 10:37:03 +0100

    updated MC demo

--- src/test/java/sbox/MastercardDemo.java
+++ src/test/java/sbox/MastercardDemo.java
@@ -7,7 +7,6 @@
 package sbox;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
@@ -20,30 +19,36 @@ import java.net.URL;
 
 import static sbox.Settings.HUB;
 
-public class CbaDemo {
+public class MastercardDemo {
+
+	private RemoteWebDriver setupSingleTest() throws MalformedURLException {
+		ChromeOptions options = new ChromeOptions();
+		options.setCapability("e34:token" , "19705d15-03b8-4f");
+		options.setCapability("video" , true);
+		options.setCapability("e34:l_testName", "Mastercard demo test");
+		RemoteWebDriver driver = new RemoteWebDriver(new URL(HUB + "/wd/hub"), options);
+		WebDriverWait wait =  new WebDriverWait(driver, 10);
+		driver.manage().window().maximize();
+		return driver;
+	}
 
 	@Test
 	public void demoSbox() throws IOException, InterruptedException {
 
-		ChromeOptions options = new ChromeOptions();
-		options.setCapability("e34:token" , "19705d15-03b8-4f");
-		options.setCapability("video" , true);
-		options.setCapability("e34:l_testName", "CBA demo test");
-		RemoteWebDriver driver = new RemoteWebDriver(new URL(HUB + "/wd/hub"), options);
-		WebDriverWait wait =  new WebDriverWait(driver, 10);
-		driver.manage().window().maximize();
-
+		RemoteWebDriver driver = setupSingleTest();
 
 		for (int i= 0; i < 3; i++) {
-			driver.get("https://www.commbank.com.au");
+			driver.get("https://www.mastercard.us/en-us.html#");
 			Thread.sleep(2000);
-			driver.findElement(By.className("log-on-text")).click();
+			driver.findElement(By.linkText("Consumers")).click();
 			Thread.sleep(2000);
-			WebElement iFrame = driver.findElement(By.className("netbank-login"));
-			driver.switchTo().frame(iFrame);
-			driver.findElement(By.id("txtMyClientNumber_field")).sendKeys("123456");
+			driver.findElement(By.linkText("Find A Card"));
 			Thread.sleep(2000);
-			driver.findElement(By.id("txtMyPassword_field")).sendKeys("12345");
+			driver.findElement(By.id("search-open")).click();
+			Thread.sleep(2000);
+			driver.findElement(By.className("query")).sendKeys("Mastercard Gold");
+			Thread.sleep(2000);
+			driver.findElement(By.className("search-btn")).click();
 			Thread.sleep(2000);
 		}
 
@@ -51,31 +56,48 @@ public class CbaDemo {
 	}
 
 
-	@Test(dataProvider = "browserProvider", dataProviderClass = TestData.class, invocationCount = 1, threadPoolSize = 20)
-	public void multiBrowserVersionTest(DesiredCapabilities caps) throws MalformedURLException, InterruptedException {
-
+	private RemoteWebDriver setupMultibrowserTest(DesiredCapabilities capabilities) throws MalformedURLException {
+		DesiredCapabilities caps = capabilities;
 		caps.setCapability("video", true);
 		caps.setCapability("e34:token", "19705d15-03b8-4f");
-		caps.setCapability("e34:l_testName", "CBA - " + caps.getBrowserName() + "  " + caps.getVersion());
+		caps.setCapability("e34:l_testName", "Mastercard - " + caps.getBrowserName() + "  " + caps.getVersion());
 		RemoteWebDriver driver = new RemoteWebDriver(new URL(HUB + "/wd/hub"), caps);
 		driver.manage().window().maximize();
+		return driver;
+	}
 
 
-		for (int i= 0; i < 5; i++) {
-			driver.get("https://www.commbank.com.au");
-			Thread.sleep(2000);
-			driver.findElement(By.className("log-on-text")).click();
-			Thread.sleep(2000);
-			WebElement iFrame = driver.findElement(By.className("netbank-login"));
-			driver.switchTo().frame(iFrame);
-			driver.findElement(By.id("txtMyClientNumber_field")).sendKeys("123456");
-			Thread.sleep(2000);
-			driver.findElement(By.id("txtMyPassword_field")).sendKeys("12345");
-			Thread.sleep(12000);
 
+
+	@Test(dataProvider = "browserProvider", dataProviderClass = TestData.class)
+	public void multiBrowserVersionTest(DesiredCapabilities caps) throws MalformedURLException, InterruptedException {
+
+		RemoteWebDriver driver = setupMultibrowserTest(caps);
+
+		for (int i= 0; i < 10; i++) {
+			driver.get("https://www.mastercard.us/en-us.html#");
+			Thread.sleep(2000);
+			driver.findElement(By.linkText("Consumers")).click();
+			Thread.sleep(2000);
+			driver.findElement(By.linkText("Find A Card"));
+			Thread.sleep(2000);
+			driver.findElement(By.id("search-open")).click();
+			Thread.sleep(2000);
+			driver.findElement(By.className("query")).sendKeys("Mastercard Gold");
+			Thread.sleep(5000);
 		}
 		driver.quit();
 	}
+
+
+
+
+
+
+
+
+
+
 
 
 
