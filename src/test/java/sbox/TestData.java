185812a724ca8c25d336d68c36e4ce6c9797a788
palotas <michael.palotas@element34.com>
2019-03-12 17:39:39 +0100

    updated test data

--- src/test/java/sbox/TestData.java
+++ src/test/java/sbox/TestData.java
@@ -6,7 +6,10 @@
 
 package sbox;
 
+import io.appium.java_client.remote.MobileCapabilityType;
 import org.openqa.selenium.Platform;
+import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.DataProvider;
 
@@ -102,19 +105,19 @@ public class TestData {
     public Object[][] getDrivers() {
 
         DesiredCapabilities chrome1 = DesiredCapabilities.chrome();
-        chrome1.setCapability("version", "70");
+        chrome1.setCapability("version", "72");
 
         DesiredCapabilities chrome2 = DesiredCapabilities.chrome();
-        chrome2.setCapability("version", "69");
+        chrome2.setCapability("version", "71");
 
         DesiredCapabilities chrome3 = DesiredCapabilities.chrome();
-        chrome3.setCapability("version", "68");
+        chrome3.setCapability("version", "70");
 
         DesiredCapabilities ff1 = DesiredCapabilities.firefox();
-        ff1.setCapability("version", "63");
+        ff1.setCapability("version", "65");
 
         DesiredCapabilities ff2 = DesiredCapabilities.firefox();
-        ff2.setCapability("version", "62");
+        ff2.setCapability("version", "64");
 
         DesiredCapabilities ff3 = DesiredCapabilities.firefox();
         ff2.setCapability("version", "63");
@@ -129,10 +132,10 @@ public class TestData {
         return new Object[][]{
                 {chrome1},
                 {chrome2},
-//                {chrome3},
+                {chrome3},
                 {ff1},
                 {ff2},
-//                {ff3},
+                {ff3},
                 {internetExplorer},
                 {edge},
                 //{safari}
@@ -206,4 +209,88 @@ public class TestData {
                 {chromeArchimedes}
         };
     }
+
+
+    @DataProvider(name = "multicaps", parallel = true)
+    public Object[][]  multicaps() {
+
+        DesiredCapabilities chrome1 = new DesiredCapabilities();
+        chrome1.setCapability("browserName", "chrome");
+        //chrome1.setCapability("e34:userId", "michel");
+        chrome1.setCapability("e34:token", "08ff3d03-543d-42");
+
+        DesiredCapabilities chrome2 = new DesiredCapabilities();
+        chrome2.setCapability("browserName", "chrome");
+        chrome2.setCapability("e34:userId", "francois");
+
+        DesiredCapabilities chrome3 = new DesiredCapabilities();
+        chrome3.setCapability("e34:userId", "remo");
+        chrome3.setCapability("browserName", "chrome");
+
+        DesiredCapabilities chrome4 = new DesiredCapabilities();
+        chrome4.setCapability("browserName", "chrome");
+
+        DesiredCapabilities chrome5 = new DesiredCapabilities();
+        chrome5.setCapability("browserName", "chrome");
+
+
+        return new Object[][]{
+                {chrome1},
+                {chrome2},
+                {chrome3},
+                {chrome4},
+                {chrome5},
+
+        };
+    }
+
+    @DataProvider(name = "mobileDataProvider", parallel = true)
+    public Object[][]  mobileDataProvider() {
+
+
+        DesiredCapabilities nexus5 = new DesiredCapabilities();
+        nexus5.setCapability(CapabilityType.PLATFORM_NAME, Platform.ANDROID);
+        nexus5.setCapability(MobileCapabilityType.PLATFORM_VERSION, "8.1");
+        nexus5.setCapability(CapabilityType.BROWSER_NAME, BrowserType.CHROME);
+        nexus5.setCapability(CapabilityType.VERSION, "70");
+        nexus5.setCapability(MobileCapabilityType.DEVICE_NAME, "Nexus 5X");
+        nexus5.setCapability("e34:token", "617a27e4-c74c-46");
+
+
+        DesiredCapabilities galaxyS8 = new DesiredCapabilities();
+        galaxyS8.setCapability(CapabilityType.PLATFORM_NAME, Platform.ANDROID);
+        galaxyS8.setCapability(MobileCapabilityType.PLATFORM_VERSION, "8.0");
+        galaxyS8.setCapability(CapabilityType.BROWSER_NAME, BrowserType.CHROME);
+        galaxyS8.setCapability(CapabilityType.VERSION, "70");
+        galaxyS8.setCapability(MobileCapabilityType.DEVICE_NAME, "Galaxy S8");
+        galaxyS8.setCapability("e34:token", "617a27e4-c74c-46");
+
+
+        DesiredCapabilities pixel2 = new DesiredCapabilities();
+        pixel2.setCapability(CapabilityType.PLATFORM_NAME, Platform.ANDROID);
+        pixel2.setCapability(MobileCapabilityType.PLATFORM_VERSION, "8.0");
+        pixel2.setCapability(CapabilityType.BROWSER_NAME, BrowserType.CHROME);
+        pixel2.setCapability(CapabilityType.VERSION, "70");
+        pixel2.setCapability(MobileCapabilityType.DEVICE_NAME, "Pixel 2");
+        pixel2.setCapability("e34:token", "617a27e4-c74c-46");
+
+
+        DesiredCapabilities nexus6P = new DesiredCapabilities();
+        nexus6P.setCapability(CapabilityType.PLATFORM_NAME, Platform.ANDROID);
+        nexus6P.setCapability(MobileCapabilityType.PLATFORM_VERSION, "8.1");
+        nexus6P.setCapability(CapabilityType.BROWSER_NAME, BrowserType.CHROME);
+        nexus6P.setCapability(CapabilityType.VERSION, "70");
+        nexus6P.setCapability(MobileCapabilityType.DEVICE_NAME, "Nexus 6P");
+        nexus6P.setCapability("e34:token", "617a27e4-c74c-46");
+
+
+
+        return new Object[][]{
+                {nexus5},
+                {galaxyS8},
+                {pixel2},
+                {nexus6P},
+        };
+    }
+
 }
