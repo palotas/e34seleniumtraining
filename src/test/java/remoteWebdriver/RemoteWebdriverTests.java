bda553d4efdc7958fbb264a8f48f78e6aa44ef76
Michael Palotas <michael.palotas@element34.com>
2020-01-29 17:40:56 +0100

    update tests

--- src/test/java/remoteWebdriver/RemoteWebdriverTests.java
+++ src/test/java/remoteWebdriver/RemoteWebdriverTests.java
@@ -7,6 +7,7 @@
 package remoteWebdriver;
 
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
@@ -20,31 +21,50 @@ import java.net.URL;
 
 public class RemoteWebdriverTests {
 
+	int LOOPS = 5;
+
 	@DataProvider(name = "capsProvider", parallel = true)
 	public Object[][] caps() {
 		return new Object[][] {
-				{DesiredCapabilities.chrome()},
-				//{DesiredCapabilities.firefox()},
-				//{DesiredCapabilities.internetExplorer()},
-				//{DesiredCapabilities.edge()}
+				{DesiredCapabilities.chrome(), "79"},
+				{DesiredCapabilities.chrome(), "78"},
+				{DesiredCapabilities.chrome(), "77"},
+				{DesiredCapabilities.firefox(), ""},
+				{DesiredCapabilities.internetExplorer(), ""},
+				{DesiredCapabilities.edge(), ""}
 		};
 	}
 
 
 	@Test(dataProvider = "capsProvider", invocationCount = 1, threadPoolSize = 5)
-	public void remoteWebdriverChromeTest(DesiredCapabilities caps) throws IOException, InterruptedException {
+	public void remoteWebdriverChromeTest(DesiredCapabilities caps, String version) throws IOException, InterruptedException {
 
 		caps.setCapability("e34:token", "c24c543b-9059-40");
 		caps.setCapability("e34:video", true);
-		caps.setCapability("e34:l_testName", "BT Demo Test");
+		caps.setCapability("e34:l_testName", "Scriptworks Demo Test: " + caps.getBrowserName() + " " + version);
+
+		caps.setVersion(version);
+
 		WebDriver driver = new RemoteWebDriver(new URL("https://vm-105.element34.net/wd/hub"), caps);
 
-		driver.get("https://bt.com");
-		Assert.assertEquals(driver.getTitle(), "Fibre Broadband, TV Packages, BT Sport & Mobile Deals | BT");
+		for (int i=0; i<LOOPS; i++) {
+			driver.get("https://scriptworks.io");
+			Assert.assertEquals(driver.getTitle(), "Home Page | Scriptworks");
 
-		Thread.sleep(5000);
-		driver.get("https://google.com");
-		Thread.sleep(5000);
+			Thread.sleep(2000);
+
+			driver.findElement(By.cssSelector("#menu-item-1639 > a")).click();
+
+			Thread.sleep(2000);
+
+			driver.findElement(By.name("first-name")).sendKeys("Michael");
+
+			Thread.sleep(2000);
+
+			driver.findElement(By.name("last-name")).sendKeys("Palotas");
+
+			Thread.sleep(5000);
+		}
 
 
 		driver.quit();
